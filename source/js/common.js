b45f7b37cccd4ca051e42c9e20038d8ba4e7db97
Andrey <soinproduction@gmail.com>
2021-06-26 14:17:00 +0300

    fix bugs

--- source/js/common.js
+++ source/js/common.js
@@ -1,6 +1,5 @@
 jQuery(function ($) {
 
-
 // -----------------  Гамбургер  --------------------
 $(".mobile-burger").click(function (event) {
   $(".mobile-menu").toggleClass("mobile-menu--active");
@@ -8,11 +7,80 @@ $(".mobile-burger").click(function (event) {
   $("body").toggleClass("body--fixed");
 });
 
-
 $('select').niceSelect();
 
 
+ // ----------------- Аккордион --------------------
+/**
+ * Классы для аккордиона
+ */
+const accordeon = {
+  CLASS: 'accordion',
+  CLASS_ACTIVE: 'accordion__active',
+}
 
+/**
+ * acc - неизменная переменная для работы с аккордионом
+ */
+const acc = document.querySelectorAll(`.${accordeon.CLASS}`);
+let openedAccordeon = null;
+
+/**
+ * использует nextElementSibling для открытия или закрытия аккордиона
+ */
+function closeAccordeon(acc) {
+  acc.nextElementSibling.style.maxHeight = 0;
+  acc.classList.remove(accordeon.CLASS_ACTIVE);
+}
+
+
+function openAccordeon(acc) {
+  acc.nextElementSibling.style.maxHeight = `${acc.nextElementSibling.scrollHeight}px`;
+  acc.classList.add(accordeon.CLASS_ACTIVE);
+}
+
+/**
+ * Проверка на открытие аккордиона !nextElementSibling!
+ */
+function isAccordeonOpen(acc) {
+  acc.nextElementSibling && !acc.nextElementSibling.style.maxHeight
+}
+
+/**
+ * Итерация, реализация переключения открытого аккордиона
+ *
+ */
+for (const accordeon of acc) {
+  accordeon.addEventListener("click", function () {
+    const currentAccordeon = this;
+
+    openedAccordeon && closeAccordeon(openedAccordeon);
+
+    if (isAccordeonOpen(currentAccordeon)) {
+      closeAccordeon(currentAccordeon);
+    } else {
+      openAccordeon(currentAccordeon);
+      openedAccordeon = currentAccordeon;
+    }
+  });
+};
+
+// --------------
+
+const accordionList = document.getElementsByClassName("accordion--multiple");
+const classNameActive = "accordion--active";
+
+for (const accordion of accordionList) {
+  accordion.addEventListener("click", function () {
+    this.classList.toggle(classNameActive);
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
 
 
 
